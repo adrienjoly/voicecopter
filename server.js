1dbbe093460898c5dc5b74a29f0c92ffdd1f5458
Gianluca Bargelli <g.bargelli@gmail.com>
2018-02-23 13:33:12 -0800

    ROULETTE MODE OVERTAKE MODE :gun: :fire: :rocket:

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// Socket.io server
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 const pdrone = require('pdrone');
-const drone = pdrone({ id: 'Team13', debug: true });
+const drone = pdrone({ id: 'Team13', debug: false });
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
@@ -18,10 +16,9 @@ http.listen(3000, function () {
 // Clap-based drone contol
 
 var clapDetector = require('clap-detector');
-clapDetector.start();
-clapDetector.onClap(function(history) {
-  console.log('CLAP => flip left');
-  drone.flip({direction: 'left'});
+clapDetector.updateConfig({
+  CLAP_ENERGY_THRESHOLD: 0.3,
+  CLAP_AMPLITUDE_THRESHOLD: 0.9
 });
 
 // Voice-based drone control
@@ -42,7 +39,7 @@ drone.on('connected', async function () {
             console.log('received take off command');;
             drone.takeOff();
             break;
-          case 'stop b****':
+          case 'enough':
             console.log('land');
             drone.land();
             break;
@@ -52,12 +49,45 @@ drone.on('connected', async function () {
             drone.fly({ gaz: 0 });
             await drone.wait(defaultWaitTime);
             break;
+          case 'go up ten':
+            drone.fly({ gaz: 100 });
+            break;
           case 'flip left':
             drone.flip({direction: 'left'});
+            break;
+          case 'flip right':
+            drone.flip({direction: 'right'});
+            break;
+          case 'flip front':
+            drone.flip({direction: 'front'});
+            break;
+          case 'flip back':
+            drone.flip({direction: 'back'});
             break;
           case 'fire':
             drone.fire();
             break;
+          case 'turn left':
+            drone.fly({yaw: -50});
+            break;
+          case 'turn right':
+            drone.fly({ yaw: 50 });
+            break;
+          default:
+            if (msg.match(/russian/ig)) {
+              clapDetector.start();
+              clapDetector.onClap(function (history) {
+                console.log('CLAP => flip left');
+                //drone.flip({direction: 'left'});
+                drone.fire();
+              });
+              console.log('russian roulette MODE ON :gun:');
+              drone.fly({yaw: -50});
+
+              // if (Math.random() >= 0.5) {
+                // drone.fire();
+              // }
+            }
         }
       // });
       // drone.on('connected', async function () {
